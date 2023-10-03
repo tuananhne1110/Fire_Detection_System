9b1873f473d28924cafee605f2a9156e4f85eb9a
PhanCongDuy <duypcse173657@gpt.edu.vn>
2023-10-03 16:42:34 +0700

    add LAB2

---
+++ W1A1/Initialization.py
@@ -0,0 +1,437 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import sklearn
+import sklearn.datasets
+
+def sigmoid(x):
+
+    s = 1/(1+np.exp(-x))
+    return s
+
+def relu(x):
+
+    s = np.maximum(0,x)
+    
+    return s
+
+def forward_propagation(X, parameters):
+   
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    # LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID
+    z1 = np.dot(W1, X) + b1
+    a1 = relu(z1)
+    z2 = np.dot(W2, a1) + b2
+    a2 = relu(z2)
+    z3 = np.dot(W3, a2) + b3
+    a3 = sigmoid(z3)
+    
+    cache = (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3)
+    
+    return a3, cache
+
+def backward_propagation(X, Y, cache):
+
+    m = X.shape[1]
+    (z1, a1, W1, b1, z2, a2, W2, b2, z3, a3, W3, b3) = cache
+    
+    dz3 = 1./m * (a3 - Y)
+    dW3 = np.dot(dz3, a2.T)
+    db3 = np.sum(dz3, axis=1, keepdims = True)
+    
+    da2 = np.dot(W3.T, dz3)
+    dz2 = np.multiply(da2, np.int64(a2 > 0))
+    dW2 = np.dot(dz2, a1.T)
+    db2 = np.sum(dz2, axis=1, keepdims = True)
+    
+    da1 = np.dot(W2.T, dz2)
+    dz1 = np.multiply(da1, np.int64(a1 > 0))
+    dW1 = np.dot(dz1, X.T)
+    db1 = np.sum(dz1, axis=1, keepdims = True)
+    
+    gradients = {"dz3": dz3, "dW3": dW3, "db3": db3,
+                 "da2": da2, "dz2": dz2, "dW2": dW2, "db2": db2,
+                 "da1": da1, "dz1": dz1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+def update_parameters(parameters, grads, learning_rate):
+    L = len(parameters) // 2 # number of layers in the neural networks
+    # Update rule for each parameter
+    for k in range(L):
+        parameters["W" + str(k+1)] = parameters["W" + str(k+1)] - learning_rate * grads["dW" + str(k+1)]
+        parameters["b" + str(k+1)] = parameters["b" + str(k+1)] - learning_rate * grads["db" + str(k+1)]  
+    return parameters
+
+def compute_loss(a3, Y):
+    m = Y.shape[1]
+    logprobs = np.multiply(-np.log(a3),Y) + np.multiply(-np.log(1 - a3), 1 - Y)
+    loss = 1./m * np.nansum(logprobs)
+    return loss
+
+def load_cat_dataset():
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    train_set_x_orig = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+    test_set_x_orig = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+    
+    train_set_x = train_set_x_orig/255
+    test_set_x = test_set_x_orig/255
+
+    return train_set_x, train_set_y, test_set_x, test_set_y, classes
+
+
+def predict(X, y, parameters):
+    m = X.shape[1]
+    p = np.zeros((1,m), dtype = int)
+    
+    # Forward propagation
+    a3, caches = forward_propagation(X, parameters)
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, a3.shape[1]):
+        if a3[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+
+    # print results
+    print("Accuracy: "  + str(np.mean((p[0,:] == y[0,:]))))
+    
+    return p
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.show()
+    
+def predict_dec(parameters, X):
+    
+    # Predict using forward propagation and a classification threshold of 0.5
+    a3, cache = forward_propagation(X, parameters)
+    predictions = (a3>0.5)
+    return predictions
+
+def load_dataset():
+    np.random.seed(1)
+    train_X, train_Y = sklearn.datasets.make_circles(n_samples=300, noise=.05)
+    np.random.seed(2)
+    test_X, test_Y = sklearn.datasets.make_circles(n_samples=100, noise=.05)
+    # Visualize the data
+    plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    train_X = train_X.T
+    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+    test_X = test_X.T
+    test_Y = test_Y.reshape((1, test_Y.shape[0]))
+    return train_X, train_Y, test_X, test_Y
+
+import numpy as np
+from dlai_tools.testing_utils import single_test, multiple_test
+
+
+def initialize_parameters_zeros_test(target):
+    layer_dims = [3,2,1]
+    expected_output = {'W1': np.array([[0., 0., 0.],
+        [0., 0., 0.]]),
+ 'b1': np.array([[0.],
+        [0.]]),
+ 'W2': np.array([[0., 0.]]),
+ 'b2': np.array([[0.]])}
+    
+    test_cases = [
+        {
+            "name":"datatype_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error":"Datatype mismatch"
+        },
+        {
+            "name": "shape_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error": "Wrong shape"
+        },
+        {
+            "name": "equation_output_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error": "Wrong output"
+        }
+    ]
+    
+    multiple_test(test_cases, target)
+    
+def initialize_parameters_random_test(target):
+    layer_dims = [3,2,1]
+    expected_output = {'W1': np.array([[ 17.88628473,   4.36509851,   0.96497468],
+        [-18.63492703,  -2.77388203,  -3.54758979]]),
+ 'b1': np.array([[0.],
+        [0.]]),
+ 'W2': np.array([[-0.82741481, -6.27000677]]),
+ 'b2': np.array([[0.]])}
+    
+    test_cases = [
+        {
+            "name":"datatype_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error":"Datatype mismatch"
+        },
+        {
+            "name": "shape_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error": "Wrong shape"
+        },
+        {
+            "name": "equation_output_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error": "Wrong output"
+        }
+    ]
+    
+    multiple_test(test_cases, target)
+    
+def initialize_parameters_he_test(target):
+    
+    layer_dims = [3, 1, 2]
+    expected_output = {'W1': np.array([[1.46040903, 0.3564088 , 0.07878985]]), 
+                       'b1': np.array([[0.]]), 
+                       'W2': np.array([[-2.63537665], [-0.39228616]]), 
+                       'b2': np.array([[0.],[0.]])}
+    
+    
+    test_cases = [
+        {
+            "name":"datatype_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error":"Datatype mismatch"
+        },
+        {
+            "name": "shape_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error": "Wrong shape"
+        },
+        {
+            "name": "equation_output_check",
+            "input": [layer_dims],
+            "expected": expected_output,
+            "error": "Wrong output"
+        }
+    ]
+    
+    multiple_test(test_cases, target)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+# from public_tests import *
+# from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+# from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# load image dataset: blue/red dots in circles
+# train_X, train_Y, test_X, test_Y = load_dataset()
+train_X, train_Y, test_X, test_Y = load_dataset()
+
+def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+    grads = {}
+    costs = [] # to keep track of the loss
+    m = X.shape[1] # number of examples
+    layers_dims = [X.shape[0], 10, 5, 1]
+    
+    # Initialize parameters dictionary.
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+    # Loop (gradient descent)
+
+    for i in range(num_iterations):
+
+        # Forward propagation: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SIGMOID.
+        a3, cache = forward_propagation(X, parameters)
+        
+        # Loss
+        cost = compute_loss(a3, Y)
+
+        # Backward propagation.
+        grads = backward_propagation(X, Y, cache)
+        
+        # Update parameters.
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        # Print the loss every 1000 iterations
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+            
+    # plot the loss
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+# GRADED FUNCTION: initialize_parameters_zeros 
+
+def initialize_parameters_zeros(layers_dims):
+    parameters = {}
+    L = len(layers_dims)            # number of layers in the network
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l - 1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+    return parameters
+
+parameters = initialize_parameters_zeros([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_zeros_test(initialize_parameters_zeros)
+
+parameters = model(train_X, train_Y, initialization = "zeros")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+print ("predictions_train = " + str(predictions_train))
+print ("predictions_test = " + str(predictions_test))
+
+
+plt.title("Model with Zeros initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+def initialize_parameters_random(layers_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the size of each layer.
+    
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
+    """
+    
+    np.random.seed(3)               # This seed makes sure your "random" numbers will be the as ours
+    parameters = {}
+    L = len(layers_dims)            # integer representing the number of layers
+    
+    for l in range(1, L):
+        #(≈ 2 lines of code)
+        # parameters['W' + str(l)] = 
+        # parameters['b' + str(l)] =
+        # YOUR CODE STARTS HERE
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        # YOUR CODE ENDS HERE
+
+    return parameters
+
+parameters = initialize_parameters_random([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_random_test(initialize_parameters_random)
+
+
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+print (predictions_train)
+print (predictions_test)
+
+
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+def initialize_parameters_he(layers_dims):
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1 # integer representing the number of layers
+     
+    for l in range(1, L + 1):
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l],layers_dims[l-1]) * np.sqrt(2/layers_dims[l-1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l],1))
+    return parameters
+
+parameters = initialize_parameters_he([2, 4, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+initialize_parameters_he_test(initialize_parameters_he)
+
+parameters = model(train_X, train_Y, initialization = "he")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+plt.title("Model with He initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
