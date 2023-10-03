99afe57eb7d896ef8457513caf0a22cbb3fcaa58
trungthanhnguyenn <trungnhtse173200@fpt.edu.vn>
2023-10-03 16:42:58 +0700

    Lab2

---
+++ W1A1/Initialization.py
@@ -0,0 +1,165 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+from public_tests import *
+from init_utils import sigmoid, relu, compute_loss, forward_propagation, backward_propagation
+from init_utils import update_parameters, predict, load_dataset, plot_decision_boundary, predict_dec
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+train_X, train_Y, test_X, test_Y = load_dataset()
+
+def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True, initialization = "he"):
+       
+    grads = {}
+    costs = [] 
+    m = X.shape[1]
+    layers_dims = [X.shape[0], 10, 5, 1]
+    
+    if initialization == "zeros":
+        parameters = initialize_parameters_zeros(layers_dims)
+    elif initialization == "random":
+        parameters = initialize_parameters_random(layers_dims)
+    elif initialization == "he":
+        parameters = initialize_parameters_he(layers_dims)
+
+
+    for i in range(num_iterations):
+
+        a3, cache = forward_propagation(X, parameters)
+        
+        cost = compute_loss(a3, Y)
+
+        grads = backward_propagation(X, Y, cache)
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+            costs.append(cost)
+
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per hundreds)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+
+def initialize_parameters_zeros(layers_dims):
+
+    parameters = {}
+    L = len(layers_dims)          
+    
+    for l in range(1, L):
+
+        parameters['W' + str(l)] = np.zeros((layers_dims[l], layers_dims[l-1]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+
+parameters = initialize_parameters_zeros([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_zeros_test(initialize_parameters_zeros)
+
+
+parameters = model(train_X, train_Y, initialization = "zeros")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+print ("predictions_train = " + str(predictions_train))
+print ("predictions_test = " + str(predictions_test))
+
+
+plt.title("Model with Zeros initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+
+
+def initialize_parameters_random(layers_dims):
+    
+    np.random.seed(3)              
+    parameters = {}
+    L = len(layers_dims)          
+    
+    for l in range(1, L):
+
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * 10
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+
+    return parameters
+
+parameters = initialize_parameters_random([3, 2, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+initialize_parameters_random_test(initialize_parameters_random)
+
+
+parameters = model(train_X, train_Y, initialization = "random")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+print (predictions_train)
+print (predictions_test)
+
+
+plt.title("Model with large random initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+def initialize_parameters_he(layers_dims):
+    
+    np.random.seed(3)
+    parameters = {}
+    L = len(layers_dims) - 1 
+     
+    for l in range(1, L + 1):
+
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l-1]) * np.sqrt(2/layers_dims[l -1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        
+    return parameters
+
+parameters = initialize_parameters_he([2, 4, 1])
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
+
+initialize_parameters_he_test(initialize_parameters_he)
+
+parameters = model(train_X, train_Y, initialization = "he")
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+plt.title("Model with He initialization")
+axes = plt.gca()
+axes.set_xlim([-1.5,1.5])
+axes.set_ylim([-1.5,1.5])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
---
+++ W1A2/Regularization.py
@@ -0,0 +1,243 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.datasets
+import scipy.io
+from reg_utils import sigmoid, relu, plot_decision_boundary, initialize_parameters, load_2D_dataset, predict_dec
+from reg_utils import compute_cost, predict, forward_propagation, backward_propagation, update_parameters
+from testCases import *
+from public_tests import *
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
+
+
+def model(X, Y, learning_rate = 0.3, num_iterations = 30000, print_cost = True, lambd = 0, keep_prob = 1):
+       
+    grads = {}
+    costs = []                           
+    m = X.shape[1]                        
+    layers_dims = [X.shape[0], 20, 3, 1]
+    parameters = initialize_parameters(layers_dims)
+
+
+    for i in range(0, num_iterations):
+        if keep_prob == 1:
+            a3, cache = forward_propagation(X, parameters)
+        elif keep_prob < 1:
+            a3, cache = forward_propagation_with_dropout(X, parameters, keep_prob)
+
+        if lambd == 0:
+            cost = compute_cost(a3, Y)
+        else:
+            cost = compute_cost_with_regularization(a3, Y, parameters, lambd)
+            
+        assert (lambd == 0 or keep_prob == 1)   
+                                                
+        if lambd == 0 and keep_prob == 1:
+            grads = backward_propagation(X, Y, cache)
+        elif lambd != 0:
+            grads = backward_propagation_with_regularization(X, Y, cache, lambd)
+        elif keep_prob < 1:
+            grads = backward_propagation_with_dropout(X, Y, cache, keep_prob)
+        
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        if print_cost and i % 10000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+        if print_cost and i % 1000 == 0:
+            costs.append(cost)
+    
+    plt.plot(costs)
+    plt.ylabel('cost')
+    plt.xlabel('iterations (x1,000)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+
+parameters = model(train_X, train_Y)
+print ("On the training set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+plt.title("Model without regularization")
+axes = plt.gca()
+axes.set_xlim([-0.75,0.40])
+axes.set_ylim([-0.75,0.65])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+def compute_cost_with_regularization(A3, Y, parameters, lambd):
+  
+    m = Y.shape[1]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    W3 = parameters["W3"]
+    
+    cross_entropy_cost = compute_cost(A3, Y) 
+    
+    L2_regularization_cost =  (1/m) * (lambd/2) * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3)))
+        
+    cost = cross_entropy_cost + L2_regularization_cost
+    
+    return cost
+
+A3, t_Y, parameters = compute_cost_with_regularization_test_case()
+cost = compute_cost_with_regularization(A3, t_Y, parameters, lambd=0.1)
+print("cost = " + str(cost))
+compute_cost_with_regularization_test(compute_cost_with_regularization)
+
+
+def backward_propagation_with_regularization(X, Y, cache, lambd):
+
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1./m * np.dot(dZ3, A2.T) + (lambd/m)*W3
+    db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
+
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T) + (lambd/m)*W2
+    db2 = 1. / m * np.sum(dZ2, axis=1, keepdims=True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T) + (lambd/m)*W1
+    db1 = 1. / m * np.sum(dZ1, axis=1, keepdims=True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+
+t_X, t_Y, cache = backward_propagation_with_regularization_test_case()
+
+grads = backward_propagation_with_regularization(t_X, t_Y, cache, lambd = 0.7)
+print ("dW1 = \n"+ str(grads["dW1"]))
+print ("dW2 = \n"+ str(grads["dW2"]))
+print ("dW3 = \n"+ str(grads["dW3"]))
+backward_propagation_with_regularization_test(backward_propagation_with_regularization)
+
+
+parameters = model(train_X, train_Y, lambd = 0.7)
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+plt.title("Model with L2-regularization")
+axes = plt.gca()
+axes.set_xlim([-0.75,0.40])
+axes.set_ylim([-0.75,0.65])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
+
+
+def forward_propagation_with_dropout(X, parameters, keep_prob = 0.5):
+    
+    np.random.seed(1)
+    
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+    W3 = parameters["W3"]
+    b3 = parameters["b3"]
+    
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+
+    D1 = np.random.rand(A1.shape[0], A1.shape[1])
+    D1 = (D1 < keep_prob).astype(int)
+    A1 = A1*D1
+    A1 = A1/keep_prob
+
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+
+    D2 = np.random.rand(A2.shape[0], A2.shape[1])
+    D2 = (D2 < keep_prob).astype(int)
+    A2 = A2*D2
+    A2 = A2 / keep_prob
+    
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+    
+    cache = (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3)
+    
+    return A3, cache
+
+
+t_X, parameters = forward_propagation_with_dropout_test_case()
+
+A3, cache = forward_propagation_with_dropout(t_X, parameters, keep_prob=0.7)
+print ("A3 = " + str(A3))
+
+forward_propagation_with_dropout_test(forward_propagation_with_dropout)
+
+
+def backward_propagation_with_dropout(X, Y, cache, keep_prob):
+
+    m = X.shape[1]
+    (Z1, D1, A1, W1, b1, Z2, D2, A2, W2, b2, Z3, A3, W3, b3) = cache
+    
+    dZ3 = A3 - Y
+    dW3 = 1./m * np.dot(dZ3, A2.T)
+    db3 = 1./m * np.sum(dZ3, axis=1, keepdims=True)
+    dA2 = np.dot(W3.T, dZ3)
+
+    dA2 = dA2 * D2
+    dA2 = dA2 / keep_prob
+
+    dZ2 = np.multiply(dA2, np.int64(A2 > 0))
+    dW2 = 1./m * np.dot(dZ2, A1.T)
+    db2 = 1./m * np.sum(dZ2, axis=1, keepdims=True)
+    
+    dA1 = np.dot(W2.T, dZ2)
+
+    dA1 = dA1 * D1
+    dA1 = dA1 / keep_prob
+
+    dZ1 = np.multiply(dA1, np.int64(A1 > 0))
+    dW1 = 1./m * np.dot(dZ1, X.T)
+    db1 = 1./m * np.sum(dZ1, axis=1, keepdims=True)
+    
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,"dA2": dA2,
+                 "dZ2": dZ2, "dW2": dW2, "db2": db2, "dA1": dA1, 
+                 "dZ1": dZ1, "dW1": dW1, "db1": db1}
+    
+    return gradients
+
+t_X, t_Y, cache = backward_propagation_with_dropout_test_case()
+
+gradients = backward_propagation_with_dropout(t_X, t_Y, cache, keep_prob=0.8)
+
+print ("dA1 = \n" + str(gradients["dA1"]))
+print ("dA2 = \n" + str(gradients["dA2"]))
+
+backward_propagation_with_dropout_test(backward_propagation_with_dropout)
+
+parameters = model(train_X, train_Y, keep_prob = 0.86, learning_rate = 0.3)
+
+print ("On the train set:")
+predictions_train = predict(train_X, train_Y, parameters)
+print ("On the test set:")
+predictions_test = predict(test_X, test_Y, parameters)
+
+
+plt.title("Model with dropout")
+axes = plt.gca()
+axes.set_xlim([-0.75,0.40])
+axes.set_ylim([-0.75,0.65])
+plot_decision_boundary(lambda x: predict_dec(parameters, x.T), train_X, train_Y)
