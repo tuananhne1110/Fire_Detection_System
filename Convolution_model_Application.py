3d29e589cb5d8289c3a86f3a980bdc1d83cfe2d9
namkhanhqq <ng.khanh1205@gmail.com>
2023-10-10 16:52:48 +0700

    nop bai mooc 4 week 1

---
+++ Convolution_model_Application.py
@@ -0,0 +1,190 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imread
+import scipy
+from PIL import Image
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.layers as tfl
+from tensorflow.python.framework import ops
+from cnn_utils import *
+from test_utils import summary, comparator
+
+np.random.seed(1)
+
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_happy_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Reshape
+Y_train = Y_train_orig.T
+Y_test = Y_test_orig.T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+index = 124
+plt.imshow(X_train_orig[index]) #display sample training image
+plt.show()
+
+
+def happyModel():
+    """
+    Implements the forward propagation for the binary classification model:
+    ZEROPAD2D -> CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> FLATTEN -> DENSE
+    
+    Note that for simplicity and grading purposes, you'll hard-code all the values
+    such as the stride and kernel (filter) sizes. 
+    Normally, functions should take these values as function parameters.
+    
+    Arguments:
+    None
+
+    Returns:
+    model -- TF Keras model (object containing the information for the entire training process) 
+    """
+    model = tf.keras.Sequential([
+            ## ZeroPadding2D with padding 3, input shape of 64 x 64 x 3
+            tfl.ZeroPadding2D(padding=(3,3), input_shape = (64,64,3)),
+            ## Conv2D with 32 7x7 filters and stride of 1
+            tfl.Conv2D(32, (7,7)),
+            ## BatchNormalization for axis 3
+            tfl.BatchNormalization(axis = -1),
+            ## ReLU
+            tfl.ReLU(),
+            ## Max Pooling 2D with default parameters
+            tfl.MaxPool2D(),
+            ## Flatten layer
+            tfl.Flatten(),
+            ## Dense layer with 1 unit for output & 'sigmoid' activation
+            tfl.Dense(1, activation="sigmoid")
+        ])
+    
+    return model
+
+
+happy_model = happyModel()
+# Print a summary for each layer
+for layer in summary(happy_model):
+    print(layer)
+    
+output = [['ZeroPadding2D', (None, 70, 70, 3), 0, ((3, 3), (3, 3))],
+            ['Conv2D', (None, 64, 64, 32), 4736, 'valid', 'linear', 'GlorotUniform'],
+            ['BatchNormalization', (None, 64, 64, 32), 128],
+            ['ReLU', (None, 64, 64, 32), 0],
+            ['MaxPooling2D', (None, 32, 32, 32), 0, (2, 2), (2, 2), 'valid'],
+            ['Flatten', (None, 32768), 0],
+            ['Dense', (None, 1), 32769, 'sigmoid']]
+    
+comparator(summary(happy_model), output)
+
+
+happy_model.compile(optimizer='adam',
+                   loss='binary_crossentropy',
+                   metrics=['accuracy'])
+
+happy_model.summary()
+
+happy_model.fit(X_train, Y_train, epochs=10, batch_size=16)
+
+happy_model.evaluate(X_test, Y_test)
+
+# Loading the data (signs)
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_signs_dataset()
+
+# Example of an image from the dataset
+index = 9
+plt.imshow(X_train_orig[index])
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+
+def convolutional_model(input_shape):
+    """
+    Implements the forward propagation for the model:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> DENSE
+    
+    Note that for simplicity and grading purposes, you'll hard-code some values
+    such as the stride and kernel (filter) sizes. 
+    Normally, functions should take these values as function parameters.
+    
+    Arguments:
+    input_img -- input dataset, of shape (input_shape)
+
+    Returns:
+    model -- TF Keras model (object containing the information for the entire training process) 
+    """
+
+    input_img = tf.keras.Input(shape=input_shape)
+    Z1 = tfl.Conv2D(8, 4, activation='linear', padding="same", strides=1)(input_img)
+    A1 = tfl.ReLU()(Z1)
+    P1 = tfl.MaxPool2D(pool_size=(8, 8), strides=(8, 8), padding='same')(A1)
+    Z2 = tfl.Conv2D(16, 2, activation='linear', padding="same", strides=1)(P1)
+    A2 = tfl.ReLU()(Z2)
+    P2 = tfl.MaxPool2D(pool_size=(4, 4), strides=(4, 4), padding='same')(A2)
+    F = tfl.Flatten()(P2)
+    outputs = tfl.Dense(6, activation='softmax')(F)
+    # YOUR CODE ENDS HERE
+    model = tf.keras.Model(inputs=input_img, outputs=outputs)
+    return model
+
+
+conv_model = convolutional_model((64, 64, 3))
+conv_model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+conv_model.summary()
+    
+output = [['InputLayer', [(None, 64, 64, 3)], 0],
+        ['Conv2D', (None, 64, 64, 8), 392, 'same', 'linear', 'GlorotUniform'],
+        ['ReLU', (None, 64, 64, 8), 0],
+        ['MaxPooling2D', (None, 8, 8, 8), 0, (8, 8), (8, 8), 'same'],
+        ['Conv2D', (None, 8, 8, 16), 528, 'same', 'linear', 'GlorotUniform'],
+        ['ReLU', (None, 8, 8, 16), 0],
+        ['MaxPooling2D', (None, 2, 2, 16), 0, (4, 4), (4, 4), 'same'],
+        ['Flatten', (None, 64), 0],
+        ['Dense', (None, 6), 390, 'softmax']]
+    
+comparator(summary(conv_model), output)
+
+
+train_dataset = tf.data.Dataset.from_tensor_slices((X_train, Y_train)).batch(64)
+test_dataset = tf.data.Dataset.from_tensor_slices((X_test, Y_test)).batch(64)
+history = conv_model.fit(train_dataset, epochs=100, validation_data=test_dataset)
+
+
+history.history
+
+
+# model was trained on. 
+df_loss_acc = pd.DataFrame(history.history)
+df_loss= df_loss_acc[['loss','val_loss']]
+df_loss.rename(columns={'loss':'train','val_loss':'validation'},inplace=True)
+df_acc= df_loss_acc[['accuracy','val_accuracy']]
+df_acc.rename(columns={'accuracy':'train','val_accuracy':'validation'},inplace=True)
+df_loss.plot(title='Model loss',figsize=(12,8)).set(xlabel='Epoch',ylabel='Loss')
+df_acc.plot(title='Model Accuracy',figsize=(12,8)).set(xlabel='Epoch',ylabel='Accuracy')
+
+
+
+
---
+++ Convolution_model_Step_by_Step_v1.py
@@ -0,0 +1,520 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from public_tests import *
+
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+np.random.seed(1)
+
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image, 
+    as illustrated in Figure 1.
+    
+    Argument:
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+    
+    Returns:
+    X_pad -- padded image of shape (m, n_H + 2 * pad, n_W + 2 * pad, n_C)
+    """
+    X_pad = np.pad(X, ((0,0), (pad,pad), (pad,pad),(0,0)), mode='constant', constant_values = (0,0))
+    return X_pad
+
+np.random.seed(1)
+x = np.random.randn(4, 3, 3, 2)
+x_pad = zero_pad(x, 3)
+print ("x.shape =\n", x.shape)
+print ("x_pad.shape =\n", x_pad.shape)
+print ("x[1,1] =\n", x[1, 1])
+print ("x_pad[1,1] =\n", x_pad[1, 1])
+
+fig, axarr = plt.subplots(1, 2)
+axarr[0].set_title('x')
+axarr[0].imshow(x[0, :, :, 0])
+axarr[1].set_title('x_pad')
+axarr[1].imshow(x_pad[0, :, :, 0])
+zero_pad_test(zero_pad)
+
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation 
+    of the previous layer.
+    
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+    
+    Returns:
+    Z -- a scalar value, the result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+    s = np.multiply(a_slice_prev,W)
+    z = np.sum([s])
+    Z = np.sum([z,np.squeeze(b)])
+    return Z
+
+np.random.seed(1)
+a_slice_prev = np.random.randn(4, 4, 3)
+W = np.random.randn(4, 4, 3)
+b = np.random.randn(1, 1, 1)
+
+Z = conv_single_step(a_slice_prev, W, b)
+print("Z =", Z)
+conv_single_step_test(conv_single_step)
+
+assert (type(Z) == np.float64), "You must cast the output to numpy float 64"
+assert np.isclose(Z, -6.999089450680221), "Wrong value"
+
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+    
+    Arguments:
+    A_prev -- output activations of the previous layer, 
+        numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+        
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function
+    """
+    # Retrieve dimensions from A_prev's shape (≈1 line)
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    (f, f, n_C_prev, n_C) = W.shape
+    
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+    
+    n_H = int((n_H_prev+(2*pad)-f)/stride)+1
+    n_W = int((n_W_prev+(2*pad)-f)/stride)+1
+    
+    Z =  np.zeros((m, n_H, n_W, n_C))
+    
+    A_prev_pad = zero_pad(A_prev, pad)
+    
+    for i in range(m):               # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]          # Select ith training example's padded activation
+        for h in range(n_H):           # loop over vertical axis of the output volume
+            # Find the vertical start and end of the current "slice" (≈2 lines)
+            vert_start = stride * h 
+            vert_end = vert_start  + f
+            
+            for w in range(n_W):       # loop over horizontal axis of the output volume
+                # Find the horizontal start and end of the current "slice" (≈2 lines)
+                horiz_start = stride * w
+                horiz_end = horiz_start + f
+                
+                for c in range(n_C):   # loop over channels (= #filters) of the output volume
+                                        
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+                    
+                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈3 line)
+                    weights = W[:, :, :, c]
+                    biases  = b[:, :, :, c]
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, weights, biases)
+    
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, hparameters)
+    
+    return Z, cache
+
+
+np.random.seed(1)
+A_prev = np.random.randn(2, 5, 7, 4)
+W = np.random.randn(3, 3, 4, 8)
+b = np.random.randn(1, 1, 1, 8)
+hparameters = {"pad" : 1,
+               "stride": 2}
+
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+z_mean = np.mean(Z)
+z_0_2_1 = Z[0, 2, 1]
+cache_0_1_2_3 = cache_conv[0][1][2][3]
+print("Z's mean =\n", z_mean)
+print("Z[0,2,1] =\n", z_0_2_1)
+print("cache_conv[0][1][2][3] =\n", cache_0_1_2_3)
+
+conv_forward_test_1(z_mean, z_0_2_1, cache_0_1_2_3)
+conv_forward_test_2(conv_forward)
+
+
+def pool_forward(A_prev, hparameters, mode = "max"):
+    """
+    Implements the forward pass of the pooling layer
+    
+    Arguments:
+    A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    hparameters -- python dictionary containing "f" and "stride"
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+    
+    Returns:
+    A -- output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters 
+    """
+    
+    # Retrieve dimensions from the input shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    
+    # Retrieve hyperparameters from "hparameters"
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+    
+    # Define the dimensions of the output
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+    
+    # Initialize output matrix A
+    A = np.zeros((m, n_H, n_W, n_C))              
+    
+    for i in range(m):               # loop over the batch of training examples
+        a_prev_slice = A_prev[i]          # Select ith training example's padded activation
+        for h in range(n_H):           # loop over vertical axis of the output volume
+            # Find the vertical start and end of the current "slice" (≈2 lines)
+            vert_start = stride * h 
+            vert_end = vert_start  + f
+            
+            for w in range(n_W):       # loop over horizontal axis of the output volume
+                # Find the horizontal start and end of the current "slice" (≈2 lines)
+                horiz_start = stride * w
+                horiz_end = horiz_start + f
+                
+                for c in range(n_C):   # loop over channels (= #filters) of the output volume
+                                        
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_slice[vert_start:vert_end,horiz_start:horiz_end,c]
+                    
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(a_slice_prev)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(a_slice_prev)
+                    else:
+                        print(mode+ "-type pooling layer NOT Defined") 
+    
+    # Store the input and hparameters in "cache" for pool_backward()
+    cache = (A_prev, hparameters)
+    return A, cache
+
+# Case 1: stride of 1
+print("CASE 1:\n")
+np.random.seed(1)
+A_prev_case_1 = np.random.randn(2, 5, 5, 3)
+hparameters_case_1 = {"stride" : 1, "f": 3}
+
+A, cache = pool_forward(A_prev_case_1, hparameters_case_1, mode = "max")
+print("mode = max")
+print("A.shape = " + str(A.shape))
+print("A[1, 1] =\n", A[1, 1])
+A, cache = pool_forward(A_prev_case_1, hparameters_case_1, mode = "average")
+print("mode = average")
+print("A.shape = " + str(A.shape))
+print("A[1, 1] =\n", A[1, 1])
+
+pool_forward_test_1(pool_forward)
+
+# Case 2: stride of 2
+print("\n\033[0mCASE 2:\n")
+np.random.seed(1)
+A_prev_case_2 = np.random.randn(2, 5, 5, 3)
+hparameters_case_2 = {"stride" : 2, "f": 3}
+
+A, cache = pool_forward(A_prev_case_2, hparameters_case_2, mode = "max")
+print("mode = max")
+print("A.shape = " + str(A.shape))
+print("A[0] =\n", A[0])
+print()
+
+A, cache = pool_forward(A_prev_case_2, hparameters_case_2, mode = "average")
+print("mode = average")
+print("A.shape = " + str(A.shape))
+print("A[1] =\n", A[1])
+
+pool_forward_test_2(pool_forward)
+
+
+def conv_backward(dZ, cache):
+    """
+    Implement the backward propagation for a convolution function
+    
+    Arguments:
+    dZ -- gradient of the cost with respect to the output of the conv layer (Z), numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward(), output of conv_forward()
+    
+    Returns:
+    dA_prev -- gradient of the cost with respect to the input of the conv layer (A_prev),
+               numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    dW -- gradient of the cost with respect to the weights of the conv layer (W)
+          numpy array of shape (f, f, n_C_prev, n_C)
+    db -- gradient of the cost with respect to the biases of the conv layer (b)
+          numpy array of shape (1, 1, 1, n_C)
+    """    
+    # Retrieve information from "cache"
+    (A_prev, W, b, hparameters) = cache
+    # Retrieve dimensions from A_prev's shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    # Retrieve dimensions from W's shape
+    (f, f, n_C_prev, n_C) = W.shape
+    
+    # Retrieve information from "hparameters"
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+    
+    # Retrieve dimensions from dZ's shape
+    (m, n_H, n_W, n_C) = dZ.shape
+    
+    # Initialize dA_prev, dW, db with the correct shapes
+    dA_prev = np.zeros(A_prev.shape)                          
+    dW = np.zeros(W.shape)
+    db = np.zeros(b.shape) # b.shape = [1,1,1,n_C]
+    
+    # Pad A_prev and dA_prev
+    A_prev_pad = zero_pad(A_prev, pad)
+    dA_prev_pad = zero_pad(dA_prev, pad)
+    
+    for i in range(m):                       # loop over the training examples
+        
+        # select ith training example from A_prev_pad and dA_prev_pad
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+        
+        for h in range(n_H):                   # loop over vertical axis of the output volume
+            for w in range(n_W):               # loop over horizontal axis of the output volume
+                for c in range(n_C):           # loop over the channels of the output volume
+                    
+                    # Find the corners of the current "slice"
+                    vert_start = stride * h 
+                    vert_end = vert_start + f
+                    horiz_start = stride * w
+                    horiz_end = horiz_start + f
+
+                    # Use the corners to define the slice from a_prev_pad
+                    a_slice = a_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+
+                    # Update gradients for the window and the filter's parameters using the code formulas given above
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:,:,:,c] * dZ[i, h, w, c]
+                    dW[:,:,:,c] += a_slice * dZ[i, h, w, c]
+                    db[:,:,:,c] += dZ[i, h, w, c]
+                    
+        # Set the ith training example's dA_prev to the unpadded da_prev_pad (Hint: use X[pad:-pad, pad:-pad, :])
+        dA_prev[i, :, :, :] = da_prev_pad[pad:-pad, pad:-pad, :]
+    
+    # YOUR CODE ENDS HERE
+    
+    # Making sure your output shape is correct
+    assert(dA_prev.shape == (m, n_H_prev, n_W_prev, n_C_prev))
+    
+    return dA_prev, dW, db
+
+np.random.seed(1)
+A_prev = np.random.randn(10, 4, 4, 3)
+W = np.random.randn(2, 2, 3, 8)
+b = np.random.randn(1, 1, 1, 8)
+hparameters = {"pad" : 2,
+               "stride": 2}
+Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+
+# Test conv_backward
+dA, dW, db = conv_backward(Z, cache_conv)
+
+print("dA_mean =", np.mean(dA))
+print("dW_mean =", np.mean(dW))
+print("db_mean =", np.mean(db))
+
+assert type(dA) == np.ndarray, "Output must be a np.ndarray"
+assert type(dW) == np.ndarray, "Output must be a np.ndarray"
+assert type(db) == np.ndarray, "Output must be a np.ndarray"
+assert dA.shape == (10, 4, 4, 3), f"Wrong shape for dA  {dA.shape} != (10, 4, 4, 3)"
+assert dW.shape == (2, 2, 3, 8), f"Wrong shape for dW {dW.shape} != (2, 2, 3, 8)"
+assert db.shape == (1, 1, 1, 8), f"Wrong shape for db {db.shape} != (1, 1, 1, 8)"
+assert np.isclose(np.mean(dA), 1.4524377), "Wrong values for dA"
+assert np.isclose(np.mean(dW), 1.7269914), "Wrong values for dW"
+assert np.isclose(np.mean(db), 7.8392325), "Wrong values for db"
+
+print("\033[92m All tests passed.")
+
+
+def create_mask_from_window(x):
+    """
+    Creates a mask from an input matrix x, to identify the max entry of x.
+    
+    Arguments:
+    x -- Array of shape (f, f)
+    
+    Returns:
+    mask -- Array of the same shape as window, contains a True at the position corresponding to the max entry of x.
+    """   
+    mask = (x == np.max(x))
+
+    return mask
+
+np.random.seed(1)
+x = np.random.randn(2, 3)
+mask = create_mask_from_window(x)
+print('x = ', x)
+print("mask = ", mask)
+
+x = np.array([[-1, 2, 3],
+              [2, -3, 2],
+              [1, 5, -2]])
+
+y = np.array([[False, False, False],
+     [False, False, False],
+     [False, True, False]])
+mask = create_mask_from_window(x)
+
+assert type(mask) == np.ndarray, "Output must be a np.ndarray"
+assert mask.shape == x.shape, "Input and output shapes must match"
+assert np.allclose(mask, y), "Wrong output. The True value must be at position (2, 1)"
+
+print("\033[92m All tests passed.")
+
+
+def distribute_value(dz, shape):
+    """
+    Distributes the input value in the matrix of dimension shape
+    
+    Arguments:
+    dz -- input scalar
+    shape -- the shape (n_H, n_W) of the output matrix for which we want to distribute the value of dz
+    
+    Returns:
+    a -- Array of size (n_H, n_W) for which we distributed the value of dz
+    """    
+    # Retrieve dimensions from shape (≈1 line)
+    (n_H, n_W) = shape
+    
+    # Compute the value to distribute on the matrix (≈1 line)
+    average = np.prod(shape)
+    
+    # Create a matrix where every entry is the "average" value (≈1 line)
+    a = (dz/average)*np.ones(shape)
+    
+    # YOUR CODE ENDS HERE
+    return a
+
+
+# In[23]:
+
+
+a = distribute_value(2, (2, 2))
+print('distributed value =', a)
+
+
+assert type(a) == np.ndarray, "Output must be a np.ndarray"
+assert a.shape == (2, 2), f"Wrong shape {a.shape} != (2, 2)"
+assert np.sum(a) == 2, "Values must sum to 2"
+
+a = distribute_value(100, (10, 10))
+assert type(a) == np.ndarray, "Output must be a np.ndarray"
+assert a.shape == (10, 10), f"Wrong shape {a.shape} != (10, 10)"
+assert np.sum(a) == 100, "Values must sum to 100"
+
+print("\033[92m All tests passed.")
+
+
+def pool_backward(dA, cache, mode = "max"):
+    """
+    Implements the backward pass of the pooling layer
+    
+    Arguments:
+    dA -- gradient of cost with respect to the output of the pooling layer, same shape as A
+    cache -- cache output from the forward pass of the pooling layer, contains the layer's input and hparameters 
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+    
+    Returns:
+    dA_prev -- gradient of cost with respect to the input of the pooling layer, same shape as A_prev
+    """
+    (A_prev, hparameters) = cache
+    
+    # Retrieve hyperparameters from "hparameters" (≈2 lines)
+    stride = hparameters["stride"]
+    f = hparameters["f"]
+    
+    # Retrieve dimensions from A_prev's shape and dA's shape (≈2 lines)
+    m, n_H_prev, n_W_prev, n_C_prev = A_prev.shape
+    m, n_H, n_W, n_C = dA.shape
+    
+    # Initialize dA_prev with zeros (≈1 line)
+    dA_prev = np.zeros(A_prev.shape)
+    
+    for i in range(m): # loop over the training examples
+        
+        # select training example from A_prev (≈1 line)
+        a_prev = A_prev[i,:,:,:]
+        
+        for h in range(n_H):                   # loop on the vertical axis
+            for w in range(n_W):               # loop on the horizontal axis
+                for c in range(n_C):           # loop over the channels (depth)
+        
+                    # Find the corners of the current "slice" (≈4 lines)
+                    vert_start  = h * stride
+                    vert_end    = h * stride + f
+                    horiz_start = w * stride
+                    horiz_end   = w * stride + f
+                    
+                    # Compute the backward propagation in both modes.
+                    if mode == "max":
+                        
+                        # Use the corners and "c" to define the current slice from a_prev (≈1 line)
+                        a_prev_slice = a_prev[ vert_start:vert_end, horiz_start:horiz_end, c ]
+                        
+                        # Create the mask from a_prev_slice (≈1 line)
+                        mask = create_mask_from_window( a_prev_slice )
+
+                        # Set dA_prev to be dA_prev + (the mask multiplied by the correct entry of dA) (≈1 line)
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, c] += mask * dA[i, h, w, c]
+                        
+                    elif mode == "average":
+                        
+                        # Get the value da from dA (≈2 line)
+                        da = dA[i, h, w, c]
+                        
+                        # Define the shape of the filter as fxf (≈1 line)
+                        shape = (f,f)
+
+                        # Distribute it to get the correct slice of dA_prev. i.e. Add the distributed value of da. (≈1 line)
+                        dA_prev[i, vert_start: vert_end, horiz_start: horiz_end, c] += distribute_value(da, shape)
+    
+    # Making sure your output shape is correct
+    assert(dA_prev.shape == A_prev.shape)
+    
+    return dA_prev
+
+np.random.seed(1)
+A_prev = np.random.randn(5, 5, 3, 2)
+hparameters = {"stride" : 1, "f": 2}
+A, cache = pool_forward(A_prev, hparameters)
+print(A.shape)
+print(cache[0].shape)
+dA = np.random.randn(5, 4, 2, 2)
+
+dA_prev1 = pool_backward(dA, cache, mode = "max")
+print("mode = max")
+print('mean of dA = ', np.mean(dA))
+print('dA_prev1[1,1] = ', dA_prev1[1, 1])  
+print()
+dA_prev2 = pool_backward(dA, cache, mode = "average")
+print("mode = average")
+print('mean of dA = ', np.mean(dA))
+print('dA_prev2[1,1] = ', dA_prev2[1, 1]) 
+
+assert type(dA_prev1) == np.ndarray, "Wrong type"
+assert dA_prev1.shape == (5, 5, 3, 2), f"Wrong shape {dA_prev1.shape} != (5, 5, 3, 2)"
+assert np.allclose(dA_prev1[1, 1], [[0, 0], 
+                                    [ 5.05844394, -1.68282702],
+                                    [ 0, 0]]), "Wrong values for mode max"
+assert np.allclose(dA_prev2[1, 1], [[0.08485462,  0.2787552], 
+                                    [1.26461098, -0.25749373], 
+                                    [1.17975636, -0.53624893]]), "Wrong values for mode average"
+print("\033[92m All tests passed.")
